fa017c5f42fdc11f28fe9d53faa149ff23b08d1d
unknown <hibabentani@gmail.com>
2024-12-19 18:56:46 +0100

    FizzBuzz(1)

---
+++ FizzBuzzTest.java
@@ -0,0 +1,37 @@
+package com.ex4;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FizzBuzzTest {
+
+
+
+        @Test
+        public void testFizzBuzz() {
+
+            assertEquals("1", FizzBuzz.de(1));
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+        /*
+        public void testFizzBuzz() {
+            assertEquals("1", FizzBuzz.de(1));
+            assertEquals("Fizz", FizzBuzz.de(3));
+            assertEquals("Buzz", FizzBuzz.de(5));
+            assertEquals("FizzBuzz", FizzBuzz.de(15));
+            assertEquals("Fizz", FizzBuzz.de(9));
+            assertEquals("Buzz", FizzBuzz.de(10));
+        }*/
+
+
